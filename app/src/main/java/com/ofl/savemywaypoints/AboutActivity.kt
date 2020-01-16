b6f82aba167341374c34ffe2aef02472f07077b1
Oliver Flöricke <tohop@posteo.de>
2020-01-16 08:24:26 +0100

    Corrected output of version number

--- app/src/main/java/com/ofl/savemywaypoints/AboutActivity.kt
+++ app/src/main/java/com/ofl/savemywaypoints/AboutActivity.kt
@@ -3,11 +3,9 @@ package com.ofl.savemywaypoints
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
-import android.view.View
 import androidx.core.app.NavUtils
 import android.view.MenuItem
 import android.widget.TextView
-import kotlinx.android.synthetic.main.activity_about.*
 
 /**
  * An example full-screen activity that shows and hides the system UI (i.e.
@@ -24,17 +22,15 @@ class AboutActivity : AppCompatActivity() {
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
 
         // initialize the screen
-        val versionTitle = findViewById<TextView>(R.id.version_swmp) as TextView
+        val versionTitle = findViewById<TextView>(R.id.about_swmp) as TextView
         versionTitle.setText(R.string.about_smwp);
 
-        val versionVersion = findViewById<TextView>(R.id.version_version) as TextView
-        versionVersion.setText("Version: " + R.string.about_version);
+        val versionVersion = findViewById<TextView>(R.id.about_version) as TextView
+        versionVersion.setText("Version: " + getString(R.string.about_current_version));
 
-        val versionCopyright = findViewById<TextView>(R.id.version_copyright) as TextView
+        val versionCopyright = findViewById<TextView>(R.id.about_copyright) as TextView
         versionCopyright.setText(R.string.about_copyright);
-
     }
-
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         val id = item.itemId
